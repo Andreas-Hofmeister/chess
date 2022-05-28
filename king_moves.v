0f3379115ad57c77f2970cb94cb10e75b4cc9456
Andreas Hofmeister <andreas.hofmeister@yahoo.de>
2022-05-28 19:22:01 +0200

    Defined the movements of the king

--- king_moves.v
+++ king_moves.v
@@ -3,10 +3,18 @@ Require Import Nat.
 From CHESS Require Export proof_tactics.
 From CHESS Require Export basics.
 From CHESS Require Export movement_basics.
-(*
+
 Inductive KingCanMakeMove (pos : Position) : 
   SquareLocation -> Move -> Prop :=
-  | KingCanMove : forall ,
+  | KingCanMove : forall from to pp c dstep,
     location_valid from -> location_valid to ->
-    are_squares_adjacent from to -> from <> to ->
-    *)
+    pos = Posn pp c dstep ->
+    SquaresAdjacent from to -> is_square_empty to pp = true ->
+    KingCanMakeMove pos from (FromTo from to)
+  | KingCanCapture : forall from to pp c dstep,
+    location_valid from -> location_valid to ->
+    pos = Posn pp c dstep ->
+    SquaresAdjacent from to -> 
+    is_square_occupied_by_enemy_piece to pp c = true ->
+    KingCanMakeMove pos from (FromTo from to).
+
