a94fc304eacf97d0dbaa4ca15adf5eb1d3671262
Andreas Hofmeister <andreas.hofmeister@yahoo.de>
2022-06-14 07:43:19 +0200

    Changed queen moves to take the player color as an argument

--- queen_moves.v
+++ queen_moves.v
@@ -7,31 +7,32 @@ From CHESS Require Export rook_moves.
 From CHESS Require Export bishop_moves.
 
 Inductive QueenCanMakeMove (pos : Position)
-: SquareLocation -> Move -> Prop :=
-  | QueenCanMoveLikeRook : forall from move,
-    RookCanMakeMove pos from move -> QueenCanMakeMove pos from move
-  | QueenCanMoveLikeBishop : forall from move,
-    BishopCanMakeMove pos from move -> QueenCanMakeMove pos from move.
+: SquareLocation -> Color -> Move -> Prop :=
+  | QueenCanMoveLikeRook : forall from c move,
+    RookCanMakeMove pos from c move -> QueenCanMakeMove pos from c move
+  | QueenCanMoveLikeBishop : forall from c move,
+    BishopCanMakeMove pos from c move -> QueenCanMakeMove pos from c move.
 
-Definition queen_moves (l : SquareLocation) (pos : Position) : (list Move) :=
-  (rook_moves l pos) ++ (bishop_moves l pos).
+Definition queen_moves (l : SquareLocation) (c : Color) (pos : Position) 
+: (list Move) :=
+  (rook_moves l c pos) ++ (bishop_moves l c pos).
 
 (*****Proofs*****)
 
-Lemma queen_moves_sound : forall move fromL pos,
+Lemma queen_moves_sound : forall move fromL c pos,
   location_valid fromL ->
-  In move (queen_moves fromL pos) -> QueenCanMakeMove pos fromL move.
+  In move (queen_moves fromL c pos) -> QueenCanMakeMove pos fromL c move.
 Proof.
-  intros move fromL pos Hv Hin.
+  intros move fromL c pos Hv Hin.
   unfold queen_moves in *. in_app_to_or. destruct Hin as [Hin | Hin].
   - apply QueenCanMoveLikeRook. apply rook_moves_sound; auto.
   - apply QueenCanMoveLikeBishop. apply bishop_moves_sound; auto.
 Qed.
 
-Lemma queen_moves_complete : forall pos fromL move,
-  QueenCanMakeMove pos fromL move -> In move (queen_moves fromL pos).
+Lemma queen_moves_complete : forall pos fromL c move,
+  QueenCanMakeMove pos fromL c move -> In move (queen_moves fromL c pos).
 Proof.
-  intros pos fromL move Hcan.
+  intros pos fromL c move Hcan.
   unfold queen_moves. in_app_to_or. inversion Hcan; subst.
   - left. apply rook_moves_complete. auto.
   - right. apply bishop_moves_complete. auto.
