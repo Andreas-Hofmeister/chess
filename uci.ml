e2cb9632bc549dac20ce2a1f701fd9e064ec4344
Andreas Hofmeister <andreas.hofmeister@yahoo.de>
2022-08-27 17:21:43 +0200

    Playing as white against the engine playing black works in PyChess

--- uci.ml
+++ uci.ml
@@ -48,15 +48,37 @@ let string_of_squareLocation sL =
     match sL with
     | Engine.Loc (rindex,findex) -> (string_of_fileindex findex)^(string_of_rankindex rindex);;
 
+let letter_of_piece (p : Engine.piece) =
+    match p with
+    | Pawn -> "p"
+    | Rook -> "r"
+    | Knight -> "n"
+    | Bishop -> "b"
+    | Queen -> "q"
+    | King -> "k";;
+
+let piece_of_letter s =
+    match s with
+    | "p" -> Engine.Pawn
+    | "r" -> Rook
+    | "q" -> Queen
+    | "n" -> Knight
+    | "b" -> Bishop
+    | "k" -> King
+    | _ -> Pawn;;
+
 let string_of_move (m : Engine.move) =
     match m with
     | FromTo (f, t) -> (string_of_squareLocation f) ^ (string_of_squareLocation t)
     | Capture (f, t) -> (string_of_squareLocation f) ^ (string_of_squareLocation t)
     | DoubleStep (f, t) -> (string_of_squareLocation f) ^ (string_of_squareLocation t)
     | EnPassant (f, t) -> (string_of_squareLocation f) ^ (string_of_squareLocation t)
-    | Promotion (_, _, _) -> "Todo"
-    | PromotionWithCapture (_, _, _) -> "Todo"
-    | Castle (_, _) -> "Todo";;
+    | Promotion (f, t, p) -> (string_of_squareLocation f) ^ (string_of_squareLocation t) ^ (letter_of_piece p)
+    | PromotionWithCapture (f, t, p) -> (string_of_squareLocation f) ^ (string_of_squareLocation t) ^ (letter_of_piece p)
+    | Castle (White, QueenSide) -> "e1c1"
+    | Castle (White, KingSide) -> "e1g1"
+    | Castle (Black, QueenSide) -> "e8c8"
+    | Castle (Black, KingSide) -> "e8g8";;
 
 let fileindex_of_string s =
     match s with
@@ -98,24 +120,53 @@ let handle_isready str =
 let movestring_list_of_string s =
     List.filter (fun x -> String.length x > 0) (List.map String.trim (String.split_on_char ' ' s));;
     
+let is_on_final_rank (loc : Engine.squareLocation) (c : Engine.color) =
+    let Engine.Loc (rank, _) = loc in
+    match c with
+    | White -> (int_of_nat rank = 7)
+    | Black -> (int_of_nat rank = 0);;
+    
+let from_to_or_capture_move pp fromLoc toLoc =
+    let toSq = Engine.get_square_by_location pp toLoc in
+    match toSq with
+    | Engine.Occupied (_,_) -> Engine.Capture (fromLoc, toLoc)
+    | _ -> Engine.FromTo (fromLoc, toLoc);;
+    
+let is_en_passant_move fromLoc toLoc =
+    let Engine.Loc (_, fromFile) = fromLoc in
+    let Engine.Loc (_, toFile) = toLoc in
+    fromFile <> toFile;;
+
+let is_double_step fromLoc toLoc =
+    let Engine.Loc (fromRank, _) = fromLoc in
+    let Engine.Loc (toRank, _) = toLoc in
+    abs((int_of_nat fromRank) - (int_of_nat toRank)) = 2;;
+
 let move_of_movestring pos mstr =
     let Engine.Posn (pp, c, pds, cavl) = pos in
     let fromLoc = Engine.Loc (rankindex_of_string (String.sub mstr 1 1), fileindex_of_string (String.sub mstr 0 1)) in
     let toLoc = Engine.Loc (rankindex_of_string (String.sub mstr 3 1), fileindex_of_string (String.sub mstr 2 1)) in
     let toSq = Engine.get_square_by_location pp toLoc in
-    match toSq with
-    | Engine.Occupied (_,_) -> Engine.Capture (fromLoc, toLoc)
-    | _ -> Engine.FromTo (fromLoc, toLoc);;
-        
-
-let letter_of_piece (p : Engine.piece) =
-    match p with
-    | Pawn -> "p"
-    | Rook -> "r"
-    | Knight -> "n"
-    | Bishop -> "b"
-    | Queen -> "q"
-    | King -> "k";;
+    let fromSq = Engine.get_square_by_location pp fromLoc in
+    match fromSq with
+    | Engine.Occupied (c, Engine.Pawn) -> 
+        if is_on_final_rank toLoc c then begin
+            let p = piece_of_letter (String.sub mstr 4 1) in
+            match toSq with
+            | Engine.Occupied (_,_) -> Engine.PromotionWithCapture (fromLoc, toLoc, p)
+            | _ -> Engine.Promotion (fromLoc, toLoc, p)
+        end else if is_en_passant_move fromLoc toLoc then Engine.EnPassant (fromLoc, toLoc)
+        else if is_double_step fromLoc toLoc then Engine.DoubleStep (fromLoc, toLoc)
+        else from_to_or_capture_move pp fromLoc toLoc
+    | Occupied (c, Engine.King) -> begin
+        match mstr with
+        | "e1c1" -> Engine.Castle (White, QueenSide)
+        | "e1g1" -> Engine.Castle (White, KingSide)
+        | "e8c1" -> Engine.Castle (Black, QueenSide)
+        | "e8g8" -> Engine.Castle (Black, KingSide)
+        | _ -> from_to_or_capture_move pp fromLoc toLoc
+        end
+    | _ -> from_to_or_capture_move pp fromLoc toLoc;;
 
 let string_of_color_and_piece (c : Engine.color) p =
     match c with
@@ -199,15 +250,17 @@ List.iter (fun m -> Printf.printf "%s\n" (string_of_move m)) (Engine.legal_moves
 
 print_endline (string_of_pos !current_position);;
 *)
-(*
-let movestrings = Scanf.sscanf "position startpos moves e2e4 f7f5 e4f5 d7d6 d1h5" "position startpos moves %s" movestring_list_of_string;;
+let movestring = "e2e4 d7d5 e4d5 h7h5 d1h5 c7c5 h5h8 d8c7 h8g8 e8d8 g8f8 d8d7 f8f7 c7e5 e1d1 b7b5 f7e7 d7e7 d2d3 e5f4 c1f4 c8e6 f4b8 e7d8 d3d4 e6g8 f1b5 g7g6 g1f3 a8b8 b1c3 g6g5 f3g5 g8h7 g5h7 d8c7 d5d6 c7d8 b5d7 a7a5 c3d5 b8c8 d5f6 c5d4 h7f8 c8c4 b2b3 c4c5 b3b4 c5b5 c2c4 a5b4 c4b5 d4d3 b5b6 d3d2 b6b7";;
 
-print_endline (String.concat " " movestrings);;
+let movestrings = List.filter (fun x -> String.length x > 0) (List.map String.trim (String.split_on_char ' ' movestring));;
 
 current_position := apply_movestrings Engine.initial_position movestrings;;
 
 print_endline (string_of_pos !current_position);;
-*)
+
+let moves = (Engine.legal_moves !current_position);;
+
+List.iter (fun m -> Printf.printf "%s\n" (string_of_move m)) (moves);;
 
 input_output_loop ();;
 
