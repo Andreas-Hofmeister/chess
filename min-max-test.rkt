66cb8fdcc56b6d414f09a3e366a74fa5f755c3bf
Andreas Hofmeister <andreas.hofmeister@yahoo.de>
2023-05-22 16:59:17 +0200

    Tested alpha beta pruning

--- min-max-test.rkt
+++ min-max-test.rkt
@@ -12,23 +12,24 @@
 
 (define initial-position (make-initial-position))
 
-(define evs (sort (evaluate-moves evaluate-opening-position
+(define test1 (pos-from-fen "r7/4k3/2n5/8/8/8/8/R3K3 w - - 0 1"))
+(define test2 (pos-from-fen "r7/4k3/8/8/8/2N5/8/R3K3 b - - 0 1"))
+
+(define evs (evaluate-moves evaluate-position
                             legal-moves
                             2
-                            initial-position)
-                  move-evaluation<=))
+                            test2))
 
 (for-each displayln evs)
 
 (displayln "")
 
-(define ab-evs (sort (evaluate-moves-alpha-beta
-                      evaluate-opening-position
+(define ab-evs (evaluate-moves-alpha-beta
+                      evaluate-position
                       legal-moves
                       2
-                      initial-position
+                      test2
                       (No-move-evaluation 'minus-infinity)
-                      (No-move-evaluation 'plus-infinity))
-                     move-evaluation<=))
+                      (No-move-evaluation 'plus-infinity)))
 
 (for-each displayln ab-evs)
