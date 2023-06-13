15d830c8639937c0587e51e463676d8ee3bd48cd
Andreas Hofmeister <andreas.hofmeister@yahoo.de>
2023-06-13 19:21:47 +0200

    Added tests for Learn Chess The Right Way Volume 1, Chapter 1

---
+++ lctrw1_ch1.rkt
@@ -0,0 +1,27 @@
+#lang typed/racket
+(require racket/match)
+
+(require "basics.rkt")
+(require "movement-basics.rkt")
+(require "legal-moves.rkt")
+(require "make-move.rkt")
+(require "move-search.rkt")
+(require "check.rkt")
+(require "fen.rkt")
+(require "uci-auxiliary.rkt")
+(require "min-max-evaluation.rkt")
+
+(: move-search (-> Position (Listof Move-evaluation)))
+(define (move-search pos)
+  (forced-mate-moves (forced-mate-search 1 pos)))
+
+(define positions (positions-from-file "../krr-test/fen_lctrw1_ch1.fen"))
+
+(define movestrings (file->lines "solutions_lctrw1_ch1.txt"))
+
+(for ([pos positions] [movestr movestrings] [index (range 1 (+ 1 (length positions)))])
+  (let* ([move (movestring->move pos movestr)]
+         [calculated-moves (move-search pos)])
+    (displayln (format "~a: ~a" index (move-evaluations-cmp-string move calculated-moves)))))
+
+
